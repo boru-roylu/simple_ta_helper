01f1238e312ff8de50cd5f3abfa70dd79726a7f1
Roy <b01501061@gmail.com>
2017-02-25 17:35:21 +0800

    add args

--- run_by_stuid.sh
+++ run_by_stuid.sh
@@ -1,11 +1,11 @@
 #! /bin/bash 
 
-if [ $# -lt 1 ]; then
+if [ $# -lt 2 ]; then
     echo "Usage:"
-    echo "$0 [student id] [delay deadline (optional)]"
+    echo "$0 [student id] [repo url list] [delay deadline (optional)]"
     echo ""
-    echo "./run_by_stuid.sh b01999001"
-    echo "or    ./run_by_stuid.sh b01999001 \"2016-01-01 00:00:00\""
+    echo "./run_by_stuid.sh b01999001 repolist"
+    echo "or    ./run_by_stuid.sh b01999001 repolist \"2016-01-01 00:00:00\""
     echo ""
     echo "Description:"
     echo "    Run by student id"
@@ -20,15 +20,17 @@ stu_id=$1 # stu_id must be above config
 . config
 . program_config
 
-if [ "$2" != "" ]; then
+if [ "$3" != "" ]; then
     git_pull=true
-    delay_time="$2"
+    repolist=$2
+    delay_time="$3"
 else
     git_pull=false
+    repolist=$2
     delay_time=""
 fi
 
-repo_name=`grep ${stu_id,,} $course/$hw/git_list/all_list | cut -d',' -f 2,2`
+repo_name=`grep ${stu_id,,} $repolist | cut -d',' -f 2,2`
 
 echo "student id: $stu_id"
 echo "repo name: $repo_name"
